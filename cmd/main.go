4c6b19b4627ef09264f8a1b1f47d2bccbbeb095f
Dmytro Ovchynnykov <odv4mail@gmail.com>
2024-08-14 13:43:03 +0300

    docker-compose & tests

--- cmd/main.go
+++ cmd/main.go
@@ -1,21 +1,37 @@
 package main
 
 import (
-	"fmt"
+	"log"
+	"os"
 
+	"github.com/Odvin/go-hex-boilerplate/internal/adapters/app/api"
 	"github.com/Odvin/go-hex-boilerplate/internal/adapters/core/arithmetic"
+	rpc "github.com/Odvin/go-hex-boilerplate/internal/adapters/framework/left/grpc"
+	"github.com/Odvin/go-hex-boilerplate/internal/adapters/framework/right/db"
 	"github.com/Odvin/go-hex-boilerplate/internal/ports"
 )
 
 func main() {
+	var err error
+
+	dbaseDriver := os.Getenv("DB_DRIVER")
+	dsourceName := os.Getenv("DS_NAME")
+
 	var core ports.ArithmaticPort
+	var appAdapter ports.APIPort
+	var dbAdapter ports.DbPort
+	var gRPCAdapter ports.GRPCPort
+
+	dbAdapter, err = db.NewAdapter(dbaseDriver, dsourceName)
+	if err != nil {
+		log.Fatalf("failed to initiate dbase connection: %v", err)
+	}
+	defer dbAdapter.CloseDbConnection()
 
 	core = arithmetic.NewAdapter()
 
-	res, err := core.Addition(2, 3)
-	if err != nil {
-		fmt.Println(err)
-	}
+	appAdapter = api.NewAdapter(dbAdapter, core)
 
-	fmt.Println(res)
+	gRPCAdapter = rpc.NewAdapter(appAdapter)
+	gRPCAdapter.Run()
 }
